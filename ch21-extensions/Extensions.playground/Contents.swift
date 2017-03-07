f05f8c7650a7a0be5a98f193db7e5125d77bb439
Andrew Weiner <weiner.andrew.j@gmail.com>
2017-03-07 16:46:11 -0600

    Ch 21 Extensions - thru Nested types and extensions

--- ch21-extensions/Extensions.playground/Contents.swift
+++ ch21-extensions/Extensions.playground/Contents.swift
@@ -12,3 +12,59 @@ protocol VehicleType {
     var hasFlatbed: Bool { get }
 }
 
+struct Car {
+    let make: String
+    let model: String
+    let year: Int
+    let color: String
+    let nickname: String
+    var gasLevel: Double {
+        willSet {
+            precondition(newValue <= 1.0 && newValue >= 0.0,
+                         "New value must be between 0 and 1.")
+        }
+    }
+}
+
+// Example of an extension conforming to a Protocol
+extension Car: VehicleType {
+    var topSpeed: Velocity { return 180 }
+    var numberOfDoors: Int { return 4 }
+    var hasFlatbed: Bool { return false }
+}
+
+extension Car {
+    init(carMake: String, carModel: String, carYear: Int) {
+        self.init(make: carMake,
+                  model: carModel,
+                  year: carYear,
+                  color: "Black",
+                  nickname: "N/A",
+                  gasLevel: 1.0)
+    }
+}
+
+var c = Car(carMake: "Ford", carModel: "Fusion", carYear: 2013)
+
+extension Car {
+    enum CarKind: CustomStringConvertible {
+        case Coupe, Sedan
+        var description: String {
+            switch self {
+            case .Coupe:
+                return "Coupe"
+            case .Sedan:
+                return "Sedan"
+            }
+        }
+    }
+    var kind: CarKind {
+        if numberOfDoors == 2 {
+            return .Coupe
+        } else {
+            return .Sedan
+        }
+    }
+}
+
+c.kind.description
