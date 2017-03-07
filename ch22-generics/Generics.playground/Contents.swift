42186f46a93f569cabce984f0726d6ea988d9bbb
Andrew Weiner <weiner.andrew.j@gmail.com>
2017-03-07 17:07:39 -0600

    Ch 22 Generics - Generic Functions and Methods

--- ch22-generics/Generics.playground/Contents.swift
+++ ch22-generics/Generics.playground/Contents.swift
@@ -11,17 +11,41 @@ struct Stack<Element> {
         }
         return items.removeLast()
     }
+    
+    func map<U>(f: (Element) -> U) -> Stack<U> {
+        var mappedItems = [U]()
+        for item in items {
+            mappedItems.append(f(item))
+        }
+        return Stack<U>(items: mappedItems)
+    }
 }
 
 var intStack = Stack<Int>()
 intStack.push(newItem: 1)
 intStack.push(newItem: 2)
+var doubledStack = intStack.map(f: { 2 * $0 })
 
 print(intStack.pop()) // Prints Optional(2)
 print(intStack.pop()) // Prints Optional(1)
 print(intStack.pop()) // Prints nil
 
+print(doubledStack.pop()) // Prints Optional(4)
+print(doubledStack.pop()) // Prints Optional(2)
+
 var stringStack = Stack<String>()
 stringStack.push(newItem: "this is a string")
 stringStack.push(newItem: "another string")
 print(stringStack.pop()) // Prints Optional("another string")
+
+func myMap<T,U>(items: [T], f: (T) -> (U)) -> [U] {
+    var result = [U]()
+    for item in items {
+        result.append(f(item))
+    }
+    return result
+}
+
+let strings = ["one", "two", "three"]
+let stringLengths = myMap(items: strings) { $0.characters.count }
+print(stringLengths) // Prints [3, 3, 5]
